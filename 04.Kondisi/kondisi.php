ede5d50aeb391b040beac09caaa26d4ba9e893e6
Chandraada <cdra73461@gmail.com>
2024-02-13 10:48:06 +0700

    upload Tugas02

---
+++ 04.Kondisi/kondisi.php
@@ -0,0 +1,46 @@
+<link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="index.css">
+    
+    <div class="container mt-5 d-flex justify-content-center">
+
+        <div class="card p-3">
+
+            <div class="d-flex align-items-center">
+
+                <div class="image">
+            <img src="ChaChan.jpg" class="rounded" width="150" >
+            </div>
+
+            <div class="ml-3 w-100">
+                
+               <h4 class="mb-0 mt-0">CHANDRA APRIANSYAH</h4>
+               <span>SOFTWARE ENGINEERING</span>
+
+
+               <div class="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
+                <div class="d-flex flex-column">
+
+                    <span class="articles">belajar php dasar</span>
+                    <span class="number1">kondisi</span>
+                    </div>
+               </div>
+              <div class="button mt-2 d-flex flex-row align-items-center">
+                <button class="btn btn-sm btn-outline-primary w-100">Chat</button>
+                <button class="btn btn-sm btn-primary w-100 ml-2">Follow</button>  
+               </div>           
+            </div>     
+    </div>       
+        </div>    
+     </div>
+</div>
+
+<?php
+$t = date("H");
+
+if ($t < "10") {
+  echo "Have a good morning!";
+} elseif ($t < "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
